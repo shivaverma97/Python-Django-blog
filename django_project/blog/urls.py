ca4ac7093c09d83c3a0b5ec9621d07a392376066
Shiva <Shiva.verma9747@gmail.com>
2020-12-18 20:43:26 +0530

    added register page and database ready for registration

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views              # . is used for current directory
 
-
 urlpatterns = [
     path('', views.home, name = 'blog-home'),
     path('about/', views.about, name = 'blog-about')
