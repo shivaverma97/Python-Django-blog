ff3d5be63ea3e09ac12ae7d399ae3cd699953045
Shiva <Shiva.verma9747@gmail.com>
2020-12-19 17:04:48 +0530

    added user profile page

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name ='login'),             #for this we dont need a diff func for login, 
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name ='logout'),          # we are just using the pre defind func of django.
     path('profile/', user_views.profile, name = 'profile')
-]
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -10,7 +10,7 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request,f'Account created for {username}! You can Login Now.')
-            return redirect(request,'login')
+            return redirect('login')
     else:
         form = UserRegisterationForm()
     return render(request,'users/register.html',{'form': form})
