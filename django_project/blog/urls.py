cfc8b8d691973490355fb0db8d22cbcf8d0b1b9c
Shiva <Shiva.verma9747@gmail.com>
2020-12-22 21:14:33 +0530

    Added all Post related features

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views              # . is used for current directory
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
-    path('about/', views.about, name = 'blog-about')
+    path('', PostListView.as_view(), name = 'blog-home'),
+    path('about/', views.about, name = 'blog-about'),
+    path('post/<int:pk>/',PostDetailView.as_view(), name = 'Post-details'),
+    path('post/new/', PostCreateView.as_view(), name = 'New-Post'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'Post-Update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = 'Post-Delete')
 ]
