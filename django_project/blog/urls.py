cff9e69dd571cc3f9bae7f567c5d02d271fc6a76
Shiva <Shiva.verma9747@gmail.com>
2020-12-26 15:06:56 +0530

    added pagination and user posts page

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views              # . is used for current directory
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'blog-home'),
+    path('user/<str:username>/', UserPostListView.as_view(), name = 'User-posts'),
     path('about/', views.about, name = 'blog-about'),
     path('post/<int:pk>/',PostDetailView.as_view(), name = 'Post-details'),
     path('post/new/', PostCreateView.as_view(), name = 'New-Post'),
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from django.contrib.auth.models import User
 
 def home(request):
     context = {'posts': Post.objects.all()}
@@ -16,6 +16,17 @@ class PostListView(LoginRequiredMixin, ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserPostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
